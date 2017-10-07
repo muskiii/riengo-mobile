59fdafd74644ff5a7958384f018b1cb336a34631
Juan Manuel Tastzian <jm@tast.com.ar>
2017-10-07 14:16:57 -0300

    pass parameters from bell creation to view

--- app/src/main/java/com/example/juanma/riengo/main/activities/ListBellsActivity.java
+++ app/src/main/java/com/example/juanma/riengo/main/activities/ListBellsActivity.java
@@ -1,7 +1,9 @@
-package com.example.juanma.riengo;
+package com.example.juanma.riengo.main.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.juanma.riengo.R;
 
 public class ListBellsActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/juanma/riengo/main/activities/MainActivity.java
+++ app/src/main/java/com/example/juanma/riengo/main/activities/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.juanma.riengo;
+package com.example.juanma.riengo.main.activities;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+
+import com.example.juanma.riengo.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override

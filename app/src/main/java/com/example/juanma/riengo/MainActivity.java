352d51110c83f9eb4bd0ccb76489138f7c50beb4
riengo@grayshirts.com.ar <oivkjrnsiqj83nr0f>
2017-10-07 13:12:26 -0300

    onesignal y mobile notifications

--- app/src/main/java/com/example/juanma/riengo/MainActivity.java
+++ app/src/main/java/com/example/juanma/riengo/MainActivity.java
@@ -6,10 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.onesignal.OneSignal;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //yepes agregado OneSignal
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
+        // Call syncHashedEmail anywhere in your app if you have the user's email.
+        // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+        OneSignal.syncHashedEmail("marianoyepes@gmail.com");
+        //yepes agregado OneSignal
+
+
         setContentView(R.layout.activity_main);
     }
 

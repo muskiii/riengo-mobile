a4a619f2db4a185a57e4b20a1cccacc5f08e1af8
Diego <diego@grayshirts.com.ar>
2017-10-07 15:37:09 -0300

    adelantos jwt

--- app/src/main/java/com/example/juanma/riengo/main/activities/ListBellsActivity.java
+++ app/src/main/java/com/example/juanma/riengo/main/activities/ListBellsActivity.java
@@ -1,9 +1,18 @@
 package com.example.juanma.riengo.main.activities;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.juanma.riengo.R;
+import com.example.juanma.riengo.main.APISDK;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ListBellsActivity extends AppCompatActivity {
 
@@ -11,5 +20,42 @@ public class ListBellsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_bells);
+        new GetBellsOperation().execute();
     }
+
+
+    public String readContent() throws IOException {
+
+        String bells  = APISDK.getBellsByOwner("owner");
+        return bells;
+
+    }
+
+    private class GetBellsOperation extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            //return readContent();
+            try {
+                return readContent();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //  return postContent(params[0]);
+            return "error";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            System.out.println(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+
 }

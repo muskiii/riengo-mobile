f5530c2c6b3eebf5e242501a8d1b5abd63d0a821
riengo@grayshirts.com.ar <oivkjrnsiqj83nr0f>
2017-10-07 16:10:46 -0300

    firebase integrado

--- app/src/main/java/com/example/juanma/riengo/MainActivity.java
+++ app/src/main/java/com/example/juanma/riengo/MainActivity.java
@@ -7,9 +7,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.onesignal.OneSignal;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +35,33 @@ public class MainActivity extends AppCompatActivity {
         //yepes agregado OneSignal
 
 
+        //yepes firebase
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id_inicio");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name_inicio");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "inicio");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+        mFirebaseAnalytics.setUserProperty("favorite_food", "pescado");
+        mFirebaseAnalytics.setUserProperty("user_email", "marianoyepes@gmail.com");
+        //yepes firebase
+
+
         setContentView(R.layout.activity_main);
     }
 
     public void createBell(View view) {
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id_ejemplo");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name_ejemplo");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+
         Intent intent = new Intent(this, CreateBellActivity.class);
         startActivity(intent);
     }

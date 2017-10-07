a746ec597d6aaec90cca058b7938f9fe524fc073
Juan Manuel Tastzian <jm@tast.com.ar>
2017-10-07 11:40:43 -0300

    add first screen mocks

--- app/src/main/java/com/example/juanma/riengo/MainActivity.java
+++ app/src/main/java/com/example/juanma/riengo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.juanma.riengo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createBell(View view) {
+        Intent intent = new Intent(this, CreateBellActivity.class);
+        startActivity(intent);
+    }
+
+    public void listBells(View view) {
+        Intent intent = new Intent(this, ListBellsActivity.class);
+        startActivity(intent);
     }
 }

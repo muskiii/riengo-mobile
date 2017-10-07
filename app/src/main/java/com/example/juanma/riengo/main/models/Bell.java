556014ae180095f510743f5cddc62e4e469e1bd9
Juan Manuel Tastzian <jm@tast.com.ar>
2017-10-07 15:41:02 -0300

    add jodatime and bell model

---
+++ app/src/main/java/com/example/juanma/riengo/main/models/Bell.java
@@ -0,0 +1,10 @@
+package com.example.juanma.riengo.main.models;
+
+import org.joda.time.DateTime;
+
+public class Bell {
+    public String name;
+    public DateTime creation_time;
+    public DateTime expiration_time;
+
+}

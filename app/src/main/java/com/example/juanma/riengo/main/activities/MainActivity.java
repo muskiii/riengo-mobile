ba5cacabb5a3fedfec5c5a3d20475309d4b17722
aspen <jmf@grayshirts.com.ar>
2017-10-07 21:48:14 -0300

    ASPEN ARREGLO TODO

--- app/src/main/java/com/example/juanma/riengo/main/activities/MainActivity.java
+++ app/src/main/java/com/example/juanma/riengo/main/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
@@ -24,6 +26,7 @@ public class MainActivity extends FragmentActivity {
     private ProfilePictureView profilePictureView;
     private TextView userNameView;
     private String id ;
+    private ProfileTracker mProfileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class MainActivity extends FragmentActivity {
         callbackManager = CallbackManager.Factory.create();
         userNameView = (TextView) findViewById(R.id.user_name);
         loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile");
         profilePictureView =  findViewById(R.id.user_pic);
         profilePictureView.setCropped(true);
     }
@@ -45,9 +49,25 @@ public class MainActivity extends FragmentActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                updateUI();
-                id = Profile.getCurrentProfile().getId();
+                if (Profile.getCurrentProfile() == null) {
+                    mProfileTracker = new ProfileTracker() {
+                        @Override
+                        protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                            Log.v("facebook - profile", currentProfile.getFirstName());
+                            id = currentProfile.getId();
+                            updateUI();
+                            mProfileTracker.stopTracking();
+                        }
+                    };
+                    // no need to call startTracking() on mProfileTracker
+                    // because it is called by its constructor, internally.
+                } else {
+                    Profile profile = Profile.getCurrentProfile();
+                        id = profile.getId();
+                        updateUI();
 
+                    Log.v("facebook - profile", profile.getFirstName());
+                }
             }
 
             @Override
@@ -57,14 +77,19 @@ public class MainActivity extends FragmentActivity {
 
             @Override
             public void onError(FacebookException error) {
+
             }
         });
 
     }
     @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        // if you don't add following block,
+        // your registered `FacebookCallback` won't be called
+        if (callbackManager.onActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
     }
     private void updateUI() {
         Profile profile = Profile.getCurrentProfile();
